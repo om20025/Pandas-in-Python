59cd0175cc22bd72026fef9ee3ae4148ac981559
Om Kalariya <79517799+om20025@users.noreply.github.com>
2022-04-04 09:17:27 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Calling DataFrame constructor
+df = pd.DataFrame()
+print(df)
+
+# list of strings
+lst = ['welcome', 'to', 'introduction', 'of ','pandas']
+
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
